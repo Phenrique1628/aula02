f7cecc957a25357b243d4c768ab98e353dda4307
Phenrique1628 <henriquepedro02964@gmail.com>
2024-09-30 17:34:36 -0400

    Tarefa UseEfect

--- src/exemplo2/index.jsx
+++ src/exemplo2/index.jsx
@@ -2,23 +2,29 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [fotos, setFotos] = useState([]);
 
   useEffect(() => { 
 
     const buscarUsuario  = async () => {
-                    = await fetch('https://jsonplaceholder.typicode.com/photos');
-        const dados =                ;
+        const resposta = await fetch('https://jsonplaceholder.typicode.com/photos');
+        const dados = await resposta.json();
         setFotos(dados);
     }
     buscarUsuario();
-  }, //complete o código);
+  }, []); 
 
   return (
     <>
       <h1>Galeria de Fotos</h1>
       <ul>
-        {}
+        {fotos.map(album => (
+        <li key={album.id}>
+        <h2>{album.title}</h2>
+        <p>{album.albumId}</p>
+        <img src={album.thumbnailurl} alt={album.title} width={100} />
+         </li>
+        ))}
       </ul>
     </>
   );
--- src/exemplo3/index.jsx
+++ src/exemplo3/index.jsx
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [usuario, setUsuario] = useState([]);
 
   useEffect(() => { 
 
-    const  ????  = async () => {
+    const buscarUsuario  = async () => {
         const resposta = await fetch('https://randomuser.me/api');
         const dados = await resposta.json();
-         //complete o código
+        setUsuario(dados)
     }
-    //complete o código
+    buscarUsuario();
   }, []);
 
   return (
